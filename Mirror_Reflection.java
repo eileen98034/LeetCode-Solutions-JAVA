2c4f1fe94a4f5c81e3609b4b67e35fbca7a3f79f
E Manton <eileen98034@users.noreply.github.com>
2020-11-17 20:23:15 -0800

    Create Mirror_Reflection.java

---
+++ Mirror_Reflection.java
@@ -0,0 +1,25 @@
+/*
+#858 : Medium
+There is a special square room with mirrors on each of the four walls.  Except for the southwest corner, 
+there are receptors on each of the remaining corners, numbered 0, 1, and 2.  The square room has walls 
+of length p, and a laser ray from the southwest corner first meets the east wall at a distance q from 
+the 0th receptor.  Return the number of the receptor that the ray meets first.  (It is guaranteed that 
+the ray will meet a receptor eventually.)
+*/
+
+class Solution {
+    public int mirrorReflection(int p, int q) {
+
+        int height = q;
+        boolean left = false;
+
+        while(height % p != 0) {
+            height += q;
+            left = !left;
+        }
+        if(left)
+            return 2;
+        else 
+            return (height/p)%2;
+    }
+}
